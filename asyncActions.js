e998293970d6a08fa02483e892c0137d9db129e4
Piyush Mehta <me@piyushmehta.com>
2019-09-25 23:59:42 +0530

    reducer

--- asyncActions.js
+++ asyncActions.js
@@ -27,3 +27,28 @@ const fetchUsersFailure = error => {
     payload: error
   };
 };
+
+const reducerName = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: ""
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
