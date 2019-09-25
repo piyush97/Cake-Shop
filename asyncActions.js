7176a39584984c0cd20639d29f9226b0e66b2fec
Piyush Mehta <me@piyushmehta.com>
2019-09-25 23:55:59 +0530

    fetch user sucess

--- asyncActions.js
+++ asyncActions.js
@@ -13,3 +13,10 @@ const fetchUsersRequest = () => {
     type: FETCH_USERS_REQUEST
   };
 };
+
+const fetchUsersSuccess = users => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  };
+};
