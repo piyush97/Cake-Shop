fa48a7303132f02788e4f6b232cbfa183f8c3361
Piyush Mehta <me@piyushmehta.com>
2019-09-25 23:51:42 +0530

    fetch

--- asyncActions.js
+++ asyncActions.js
@@ -3,3 +3,7 @@ const initialState = {
   users: [],
   error: ""
 };
+
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
